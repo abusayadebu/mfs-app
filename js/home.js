a7e18a6e6696095ce2ef22e0cb8bbce8902ef0db
abusayadebu <abusayad743@gmail.com>
2025-07-29 00:09:13 +0600

    add money form functionality adde

--- js/home.js
+++ js/home.js
@@ -1,1 +1,20 @@
-console.log("hompage added")
+document.getElementById("add-money").addEventListener("click", function(event){
+    event.preventDefault();
+    const amountValue = document.getElementById("amount-value").value;
+    const amountValueNumber = Number(amountValue);
+
+    const pinNumber = document.getElementById("pin-number").value;
+    const currentBalence = document.getElementById("current-balance").innerText;
+    const currentBalanceNumber = Number(currentBalence);
+
+    // validation here---
+    if(pinNumber == 1234){
+        const takeBalance = currentBalanceNumber + amountValueNumber;
+        document.getElementById("current-balance").innerText = takeBalance; //updated balance show here
+    }
+    else{
+        alert("Credencial wrong")
+    }
+
+
+})
